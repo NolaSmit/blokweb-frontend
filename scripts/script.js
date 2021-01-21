fb582cb444e5217b6769d65bd2be853112aaeb4a
NolaSmit <74206066+NolaSmit@users.noreply.github.com>
2021-01-21 16:29:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+"use strict" ;
+
+console.log('Im begging you please work')
+
+var menuButton = document.querySelector('#nav-button') ;
+
+function toggleMenu() {
+    document.nav.classList.toggle('togglenav');
+}
+
+menuButton.addEventListener('click', toggleMenu);
